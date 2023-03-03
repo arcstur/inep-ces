06b39c65bad8710a7f7b21f3c407132af93fc166
arcstur <arcstur@gmail.com>
2023-03-03 09:11:41 -0300

    release: first draft

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # inep-analise-ces
 Análise dos microdados do Censo da Educação Superior do INEP
+
+Por padrão a análise é feita para o curso de Arquitetura e Urbanismo, no estado do Rio Grande do Sul. Isso pode ser configurado com variáveis de ambiente.
+
+## Cálculo de concluintes em um dado ano
+
+Baixe o arquivo dos microdados em um csv, configure as variáveis de ambiente, e rode o script `concluintes.py`
+
+## Configuração
+
+As seguintes variáveis de ambiente definem o programa.
+- `CSV_FILENAME` (default `2021.csv`): nome do arquivo (deve ser um microdado do CES do INEP)
+- `SG_UF` (default `RS`): sigla da UF
+- `NO_CURSO` (default `Arquitetura E Urbanismo`): nome do curso
---
+++ concluintes.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+
+QT_COLUMNS = ["QT_CONC", "QT_CONC_FEM", "QT_CONC_MASC"]
+FILTER_COLUMNS = [
+    "SG_UF",
+    "NO_CURSO",
+]
+NO_CURSO = os.environ.get("NO_CURSO", "Arquitetura E Urbanismo")
+SG_UF = os.environ.get("SG_UF", "RS")
+
+
+def main():
+    filename = os.environ.get("CSV_FILENAME", "2021.csv")
+    df = get_dataframe(filename)
+
+    for year in df["NU_ANO_CENSO"].unique():
+        df_year = df.loc[df["NU_ANO_CENSO"] == year]
+        agg_dict = {column: "first" for column in FILTER_COLUMNS} | {
+            column: "sum" for column in QT_COLUMNS
+        }
+        results = df_year.groupby("NU_ANO_CENSO").agg(agg_dict)
+        print(results)
+
+    results.to_excel(f"concluintes.{NO_CURSO}.xlsx")
+
+
+def get_dataframe(filename, usecols=None):
+    usecols = ["NU_ANO_CENSO"] + FILTER_COLUMNS + QT_COLUMNS
+    df = pd.read_csv(
+        filename,
+        delimiter=";",
+        encoding="ISO-8859-1",
+        # otherwise it complains about columns having mixed types
+        low_memory=False,
+        usecols=usecols,
+    )
+    df = df.loc[df["SG_UF"] == SG_UF]
+    df = df.loc[df["NO_CURSO"] == NO_CURSO]
+    return df
+
+
+if __name__ == "__main__":
+    main()
